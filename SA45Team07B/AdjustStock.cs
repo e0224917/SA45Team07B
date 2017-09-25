c98b7f8319795d0b02c386d24be9a8d87d156e04
Ton That Minh Nhat <e0224917@u.nus.edu>
2017-09-25 15:58:43 +0800

    Initiate AdjustStock form

--- SA45Team07B/AdjustStock.cs
+++ SA45Team07B/AdjustStock.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -10,9 +11,78 @@ namespace SA45Team07B
 {
     public partial class AdjustStock : SA45Team07B.BaseForm
     {
+        Book mBook;
+        bool discontinued;
+
         public AdjustStock()
         {
             InitializeComponent();
+            mBook = null;
+        }
+
+        private void txtRFID_TextChanged(object sender, EventArgs e)
+        {
+            if (txtRFID.Text.Length == 10)
+            {
+
+                mBook = DataService.GetBookFromRFID(txtRFID.Text);
+
+                if (ValidateRFID())
+                {
+                    txtbBookTitle.Text = mBook.BookTitle;
+                    discontinued = DataService.GetRFIDStatus(txtRFID.Text);
+                    rbYes.Checked = discontinued;
+                    rbNo.Checked = !discontinued;
+                }
+            }
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+            if (!ValidateRFID()) return;
+
+            if (discontinued == rbYes.Checked)
+            {
+                MessageBox.Show("No changes");
+                DialogResult = DialogResult.Cancel;
+            }
+            else
+            {
+                DataService.MakeRFIDAdjusment(txtRFID.Text, discontinued, txtbRemarks.Text);
+                MessageBox.Show("Update Success!");
+                DialogResult = DialogResult.OK;
+            }
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void txtRFID_Validating(object sender, CancelEventArgs e)
+        {
+            ValidateRFID();
+        }
+
+        private bool ValidateRFID()
+        {
+            if (txtRFID.Text.Length < 10)
+            {
+                epRFID.SetError(txtRFID, "Wrong RFID");
+                return false;
+            }
+            else if (mBook == null)
+            {
+                epRFID.SetError(txtRFID, "No record");
+                return false;
+            }
+            epRFID.SetError(txtRFID, "");
+            return true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
         }
     }
 }
---
+++ SA45Team07B/SearchMemberControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SA45Team07B
+{
+    public partial class SearchMemberControl : SA45Team07B.SearchControl
+    {
+        public SearchMemberControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
