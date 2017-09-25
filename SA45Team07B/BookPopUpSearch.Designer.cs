e8a6592994ce748193b23f9aca60d7d3f0aa34a5
CK Tan <tanck105@hotmail.com>
2017-09-25 09:45:36 +0800

    PopUpSearch

--- SA45Team07B/BookPopUpSearch.Designer.cs
+++ SA45Team07B/BookPopUpSearch.Designer.cs
@@ -28,22 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BookPopUpSearch));
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.dataGridViewBookList = new System.Windows.Forms.DataGridView();
+            this.lblSelectBook = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBookList)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.White;
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.Orange;
+            this.btnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.Black;
+            this.btnCancel.Location = new System.Drawing.Point(771, 560);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(137, 81);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.White;
+            this.btnOK.FlatAppearance.BorderColor = System.Drawing.Color.Orange;
+            this.btnOK.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnOK.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.ForeColor = System.Drawing.Color.Black;
+            this.btnOK.Location = new System.Drawing.Point(544, 560);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(137, 81);
+            this.btnOK.TabIndex = 17;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // dataGridViewBookList
+            // 
+            this.dataGridViewBookList.AllowUserToAddRows = false;
+            this.dataGridViewBookList.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.dataGridViewBookList.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewBookList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridViewBookList.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewBookList.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridViewBookList.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Orange;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewBookList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewBookList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.ControlLight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewBookList.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridViewBookList.EnableHeadersVisualStyles = false;
+            this.dataGridViewBookList.GridColor = System.Drawing.Color.White;
+            this.dataGridViewBookList.Location = new System.Drawing.Point(47, 91);
+            this.dataGridViewBookList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridViewBookList.MultiSelect = false;
+            this.dataGridViewBookList.Name = "dataGridViewBookList";
+            this.dataGridViewBookList.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Orange;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewBookList.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridViewBookList.RowHeadersVisible = false;
+            this.dataGridViewBookList.RowHeadersWidth = 5;
+            this.dataGridViewBookList.RowTemplate.Height = 24;
+            this.dataGridViewBookList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewBookList.Size = new System.Drawing.Size(1154, 360);
+            this.dataGridViewBookList.TabIndex = 16;
+            // 
+            // lblSelectBook
+            // 
+            this.lblSelectBook.AutoSize = true;
+            this.lblSelectBook.BackColor = System.Drawing.Color.Transparent;
+            this.lblSelectBook.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelectBook.Location = new System.Drawing.Point(42, 37);
+            this.lblSelectBook.Name = "lblSelectBook";
+            this.lblSelectBook.Size = new System.Drawing.Size(211, 28);
+            this.lblSelectBook.TabIndex = 15;
+            this.lblSelectBook.Text = "Select a book from list:";
             // 
             // BookPopUpSearch
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1267, 966);
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.dataGridViewBookList);
+            this.Controls.Add(this.lblSelectBook);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "BookPopUpSearch";
-            this.Text = "BookPopUpSearch";
+            this.Text = "Book Search";
+            this.Load += new System.EventHandler(this.BookPopUpSearch_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBookList)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.DataGridView dataGridViewBookList;
+        private System.Windows.Forms.Label lblSelectBook;
     }
 }
--- SA45Team07B/MemberPopUpSearchcs.Designer.cs
+++ SA45Team07B/MemberPopUpSearchcs.Designer.cs
@@ -33,23 +33,23 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MemberPopUpSearch));
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblSelectMember = new System.Windows.Forms.Label();
             this.dataGridViewMemberList = new System.Windows.Forms.DataGridView();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMemberList)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblSelectMember
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(30, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 28);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Select member from list:";
+            this.lblSelectMember.AutoSize = true;
+            this.lblSelectMember.BackColor = System.Drawing.Color.Transparent;
+            this.lblSelectMember.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelectMember.Location = new System.Drawing.Point(30, 27);
+            this.lblSelectMember.Name = "lblSelectMember";
+            this.lblSelectMember.Size = new System.Drawing.Size(238, 28);
+            this.lblSelectMember.TabIndex = 1;
+            this.lblSelectMember.Text = "Select a member from list:";
             // 
             // dataGridViewMemberList
             // 
@@ -146,7 +146,7 @@
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.dataGridViewMemberList);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSelectMember);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MemberPopUpSearch";
@@ -160,7 +160,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSelectMember;
         private System.Windows.Forms.DataGridView dataGridViewMemberList;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnCancel;
